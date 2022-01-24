106d6b17b652fe4e61cce0fb0f572cc826b0479d
Batou1406 <55761147+Batou1406@users.noreply.github.com>
2022-01-24 14:12:48 +0100

    Update todo2_2.m

--- todo2_2.m
+++ todo2_2.m
@@ -1,1 +1,17 @@
-[sys x, sys y, sys z, sys roll] = rocket.decompose(sys, xs, us)
+clear all
+close all
+
+Ts = 1/20;
+rocket = Rocket(Ts);
+[xs, us] = rocket.trim() % Compute steady−state for which 0 = f(xs,us)
+
+w = [0,0,0];
+phi = [0,0,0];
+v = [0,0,0];
+p = [0,0,0];
+xs = [w, phi, v, p]';
+ 
+sys = rocket.linearize(xs, us)
+
+
+[sys_x, sys_y, sys_z, sys_roll] = rocket.decompose(sys, xs, us)
